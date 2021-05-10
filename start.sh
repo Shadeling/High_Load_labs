3d07f7b6fddac970ce394e723a7975960ebdc3f9
Shadeling <mironow.slav@yandex.ru>
2021-05-10 20:09:56 +0300

    lab1 final

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo ./hl_mai_lab_01 --host=127.0.0.1 --port=8080 --login=lab --password=12345 --database=itlabs
+sudo ./CMakeFiles/hl_mai_lab_01 --host=127.0.0.1 --port=8080 --login=lab --password=12345 --database=itlabs
